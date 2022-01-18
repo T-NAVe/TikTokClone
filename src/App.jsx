ae5278cdffaaddad15434eb503c8690b95b449c4
Luca de Acha <lucadeacha@gmail.com>
2022-01-18 06:36:08 -0300

    creating project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App () {
+
+  return (
+    <div className='App'>
+      <main style={{color:"white"}}>
+      TikTok Clone
+      </main>
+    </div>
+  )
+}
+
+export default App
