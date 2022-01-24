ab06da331fb08f5b0f54dea15a477b90bb253aa8
Luca de Acha <lucadeacha@gmail.com>
2022-01-24 18:19:28 -0300

    feat(Root): added svgicons and props

--- src/components/VideoPlayer/VideoPlayerActions.jsx
+++ src/components/VideoPlayer/VideoPlayerActions.jsx
@@ -1,17 +1,23 @@
-import { Heart } from '../icons/Heart'
+import { Heart } from '../Icons/Heart.jsx'
+import { Share } from '../Icons/Share.jsx'
+import { Comment } from '../Icons/Comment.jsx'
+
 import styles from './styles.module.css'
 
-export default function VideoPlayerActions () {
+export default function VideoPlayerActions ({likes = 0, comments = 0, shares = 0, hearted = false }) {
     return (
         <aside className={styles.actions}>
             <div className={styles.action}>
                 <Heart/>
+                <strong title='Like'>{likes}</strong>
             </div>
             <div className={styles.action}>
-                <Heart/>
+                <Comment/>
+                <strong title='Comments'>{comments}</strong>
             </div>
             <div className={styles.action}>
-                <Heart/>
+                <Share/>
+                <strong title='Shares'>{shares}</strong>
             </div>
         </aside>
     )
